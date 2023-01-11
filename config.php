beb417daef5af867d818c8a6a5f07967981d000e
xkubiakkx <117862039+xkubiakkx@users.noreply.github.com>
2023-01-11 17:41:05 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	
-	$databaseHost = 'mysql1.mydevil.net';
-	$databaseName = 'm14577_smile';
-	$databaseUser = 'm14577_smile';
-	$databasePass = '82t09b2-LSxjKOG+Slb5=,ekuZ0p~5';
+	$databaseHost = 'server';
+	$databaseName = 'name';
+	$databaseUser = 'username';
+	$databasePass = 'password';
 
 ?>
