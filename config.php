2dda40a3cabbfc62b1ff27c45ceb2bfcc97ed0e5
AMW-Pay-Project <23445@edu.amw.gdynia.pl>
2023-01-11 15:41:45 +0100

    PP-23 Logowanie 2

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	
-	$databaseHost = '';
-	$databaseName = '';
-	$databaseUser = '';
-	$databasePass = '';
+	$databaseHost = 'mysql1.mydevil.net';
+	$databaseName = 'm14577_smile';
+	$databaseUser = 'm14577_smile';
+	$databasePass = '82t09b2-LSxjKOG+Slb5=,ekuZ0p~5';
 
 ?>
--- model/guest/remind_password.php
+++ model/guest/remind_password.php
@@ -61,33 +61,40 @@
 			
 			if($fetch['activation_code'] == $code) {
 				$pass = $core->clearText($_POST['PASS']);
+				$passrepeat = $core->clearText($_POST['PASSREPEAT']);
 				
-				if(preg_match("#.*^(?=.{10,32})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#", $pass)) {
-					$newQuery = $database->prepare("UPDATE `users` SET `password`=:pass WHERE `id`=:id");
-					$newQuery->bindValue("pass", sha1(md5($pass)), PDO::PARAM_STR);
-					$newQuery->bindValue("id", $fetch['id'], PDO::PARAM_INT);
-					$newQuery->execute();
+				if($pass == $passrepeat) {
+					if(preg_match("#.*^(?=.{10,32})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#", $pass)) {
+						$newQuery = $database->prepare("UPDATE `users` SET `password`=:pass WHERE `id`=:id");
+						$newQuery->bindValue("pass", sha1(md5($pass)), PDO::PARAM_STR);
+						$newQuery->bindValue("id", $fetch['id'], PDO::PARAM_INT);
+						$newQuery->execute();
 				
-					$view->load('info');
-						$view->add('title', ' Hasło zmienione');
-						$view->add('header', 'Hasło zmienione!');
-						$view->add('info', 'Hasło zostało zmienione poprawnie');
-						$view->add('back', 'index.php?page=login');
-					$view->out();
+						$view->load('info');
+							$view->add('title', ' Hasło zmienione');
+							$view->add('header', 'Hasło zmienione!');
+							$view->add('info', 'Hasło zostało zmienione poprawnie');
+							$view->add('back', 'index.php?page=login');
+						$view->out();
+					} else {
+						$view->load("remind_password_new_error");
+							$view->add("mail", $mail);
+							$view->add("code", $code);
+							$view->add("error", "Hasło musi mieć od 10 do 32 znaków, małą i dużą literę oraz znak specjalny!");
+						$view->out();
+					}
 				} else {
-					$view->load('info');
-						$view->add('title', ' Błąd zmiany hasła');
-						$view->add('header', 'Błąd zmiany hasła!');
-						$view->add('info', 'Hasło musi mieć od 10 do 32 znaków, małą i dużą literę oraz znak specjalny!');
-						$view->add('back', 'index.php?page=remind_password');
+					$view->load("remind_password_new_error");
+						$view->add("mail", $mail);
+						$view->add("code", $code);
+						$view->add("error", "Hasła muszą być takie same!");
 					$view->out();
 				}
 			} else {
-				$view->load('info');
-					$view->add('title', ' Błędny kod');
-					$view->add('header', 'Błędny kod!');
-					$view->add('info', 'Podany kod weryfikacyjny jest błędny');
-					$view->add('back', 'index.php?page=remind_password');
+				$view->load("remind_password_new_error");
+					$view->add("mail", $mail);
+					$view->add("code", $code);
+					$view->add("error", "Podany kod weryfikacyjny jest błędny!");
 				$view->out();
 			}
 		}
